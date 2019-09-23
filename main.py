1656f814210ee8f9807cfc9e7486e91575e5aed6
zebralt <15049142+Zebralt@users.noreply.github.com>
2019-09-23 20:50:55 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,20 @@
+from functools import partial, wraps
+
+from flask import Flask, request
+
+
+app = Flask(__name__)
+app.post = partial(app.route, methods=['POST'])
+app.get = partial(app.route, methods=['GET'])
+
+
+@app.route('/list')
+def list_songs():
+    return ''
+  
+  
+@app.route('/download')
+@ratelimit(10, every=1)
+def download_song():
+    ...
+    
